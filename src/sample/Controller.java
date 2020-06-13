9d819f6e2d8705067c991c19bc17c69466e1a56e
nastasyanky <ushakova_nastena@mail.ru>
2020-06-13 11:36:00 +0300

    Ush_6_2.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
+
 public class Controller {
     public TextField tx_array;
     public TextField tx_bubble;
@@ -17,55 +20,46 @@ public class Controller {
     public ListView list_view;
     private Context_R context;
     int n=0;
-    StringBuffer sb_bubble=new StringBuffer();
-    StringBuffer sb_select=new StringBuffer();
-    StringBuffer sb_insert=new StringBuffer();
-    StringBuffer sb=new StringBuffer();
-    private int[] array_n = new int[5];
-    public int[]array_b=new int[]{1,5,9,3,6};
-
+    ArrayList<String> al =new ArrayList<String>();
+    ArrayList<String> al_bubble =new ArrayList<String>();
+    ArrayList<String> al_select =new ArrayList<String>();
+    ArrayList<String> al_insert =new ArrayList<String>();
+    private int[] array_n = new int[7];
 
     public void on_array(ActionEvent actionEvent) {
-        sb.setLength(0);
+        al.clear();
         for (int i = 0; i < array_n.length; i++) {
             array_n[i] = ((int) (Math.random() * 11) + 1);
-            sb.append(String.valueOf(array_n[i]+" | "));
+            al.add(String.valueOf(array_n[i]));
             }
-        tx_array.setText(sb.toString());
-
+        tx_array.setText(al.toString());
     }
-
     public void bubbleSort(ActionEvent actionEvent) {
-        sb_bubble.setLength(0);
+        al_bubble.clear();
         context = new Context_R(new Bubble_Sort_R());
         context.sortArray_R(array_n);
         for(int i = 0; i <  array_n.length; i++) {
-            sb_bubble.append(String.valueOf(array_n[i]+" | "));
+            al_bubble.add(String.valueOf(array_n[i]));
         }
-        tx_bubble.setText(sb_bubble.toString());
+        tx_bubble.setText(al_bubble.toString());
         }
-
 
     public void selectionSort(ActionEvent actionEvent) {
-        sb_select.setLength(0);
+        al_select.clear();
         context = new Context_R(new Selection_Sort_R());
         context.sortArray_R(array_n);
         for(int i = 0; i <  array_n.length; i++) {
-            sb_select.append(String.valueOf(array_n[i]+" | "));
+            al_select.add(String.valueOf(array_n[i]));
         }
-        tx_selection.setText(sb_select.toString());
+        tx_selection.setText(al_select.toString());
     }
-
-
     public void insertionSort(ActionEvent actionEvent) {
-        sb_insert.setLength(0);
+        al_insert.clear();
         context = new Context_R(new Insertion_Sort_R());
         context.sortArray_R(array_n);
         for(int i = 0; i <  array_n.length; i++) {
-            sb_insert.append(String.valueOf(array_n[i]+" | "));
+            al_insert.add(String.valueOf(array_n[i]));
         }
-        tx_insertion.setText(sb_insert.toString());
+        tx_insertion.setText(al_insert.toString());
     }
-
-
 }
